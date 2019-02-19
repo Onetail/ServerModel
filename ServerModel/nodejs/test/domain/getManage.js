0b2d0876d4979d7b22b97fbacbdde8e953c88fbd
onetail <onetail@github.com>
2019-02-19 21:35:57 +0800

    add unit test done() in every it

--- ServerModel/nodejs/test/domain/getManage.js
+++ ServerModel/nodejs/test/domain/getManage.js
@@ -15,46 +15,51 @@ describe(`getManage`,()=>{
     var app , 
         server ;
 
-    beforeEach(()=>{ 
+    before(()=>{ 
         app = express()
         server = http.createServer(app)
         // server.listen(8501)
-    })
-    before(()=>{
         chai.use(chaiHttp)
     })
     
-    after(()=>{
-        server.close()    
-    })
-
     describe(`#domain`,()=>{
-        it(`["test"] == []`,()=>{
+        it(`["test"] == []`,(done)=>{
             expect(gm.domain(["test"],app).toString())
             .to 
             .equal("")
+            done()
         })
-        it(`["/ppa/test/user"] == []`,()=>{
+        it(`["/ppa/test/user"] == []`,(done)=>{
             expect(gm.domain(["/ppa/test/user"],app).toString())
             .to 
             .equal("")
+            done()
         })
-        it(`["/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/user"] == ["/ObjectModel/test/user"]`,()=>{
+        it(`["/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/user"] == ["/ObjectModel/test/user"]`,(done)=>{
             expect(gm.domain(["/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/user"],app).toString())
             .to
             .equal("/ObjectModel/test/user")
+            done()
         })
-        it.skip(`["/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/action"] == ["/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/action"]`,()=>{
+        it.skip(`["/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/action"] == ["/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/action"]`,(done)=>{
             expect(gm.domain(["/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/action"],app).toString())
             .to.equal("/Users/chuyuwei/Documents/programming/project/myChicks/backend/backend/ObjectModel/test/action")
-            throw new Error(global.POSTFILEENDWITH)
-            
+            done()
         })
     })
     describe(`#Atdomain`,()=>{
         
     })
     describe(`#defaultPage`,()=>{
-
+        it(`/*/ = 200 && res.body == undefined`,(done)=>{
+            gm.defaultPage(app)
+            chai.request(server)
+            .get("/*")
+            .end(function(err,res,body){
+                expect(200).to.equal(res.status)
+                expect().to.equal(res.body.size)
+                done()
+            })
+        })
     })
 })
--- ServerModel/nodejs/test/domain/postManage.js
+++ ServerModel/nodejs/test/domain/postManage.js
@@ -15,56 +15,55 @@ describe("PostManager",()=>{
     var app , 
         server ;
 
-    beforeEach(()=>{ 
+    before(()=>{ 
         app = express()
         server = http.createServer(app)
-        // server.listen(8501)
+        chai.use(chaiHttp)  
     })
-    before(()=>{
-        chai.use(chaiHttp)
-    })
-    
-    after(()=>{
-        server.close()    
-    });
     
     describe("#post Domain",()=>{
-        it(`domain(["test"],app)[0] == "/test" `,()=>{
+        it(`domain(["test"],app)[0] == "/test" `,(done)=>{
             
             expect(pm.domain(["test"],app)[0]).to.equal("/test")
             chai.request(server)
             .post("/test")
             .end(function(err,res,body){
                 expect(200).to.equal(res.status)
+                done()
             })
-            
         })
-        it(`domain(["/test"],app)[0] == "/test"`,()=>{
+        it(`domain(["/test"],app)[0] == "/test"`,(done)=>{
             expect(pm.domain(["/test",""],app)[0]).to.equal("/test")
+            done()
         })
-        it(`domain(["dir/test"],app)[0] == "/test"`,()=>{
+        it(`domain(["dir/test"],app)[0] == "/test"`,(done)=>{
             expect(pm.domain(["dir/test"],app)[0]).to.equal("/test")
+            done()
         })
-        it(`domain(["/dir/test"],app)[0] == "/test"`,()=>{
+        it(`domain(["/dir/test"],app)[0] == "/test"`,(done)=>{
             expect(pm.domain(["/dir/test"],app)[0]).to.equal("/test")
+            done()
         })
-        it(`domain(["${global.Server.SERVERLOCALE}/dir/test"],app)[0] == "${global.Server.SERVERLOCALE}/dir/test"`,()=>{
+        it(`domain(["${global.Server.SERVERLOCALE}/dir/test"],app)[0] == "${global.Server.SERVERLOCALE}/dir/test"`,(done)=>{
             expect(
                 pm.domain([`${global.Server.SERVERLOCALE}/dir/test`],app)[0]
             ).to.equal(`${global.Server.SERVERLOCALE}/dir/test`)
+            done()
         })        
-        it(`domain(["/a${global.Server.SERVERLOCALE}/dir/test"],app)[0] == "${global.Server.SERVERLOCALE}/dir/test"`,()=>{
+        it(`domain(["/a${global.Server.SERVERLOCALE}/dir/test"],app)[0] == "${global.Server.SERVERLOCALE}/dir/test"`,(done)=>{
             expect(
                 pm.domain([`/a${global.Server.SERVERLOCALE}/dir/test`],app)[0]
             ).to.equal(`${global.Server.SERVERLOCALE}/dir/test`)
+            done()
         })
-        it(`domain(["dir/test","/a${global.Server.SERVERLOCALE}/dir/test"],app) == "/test,${global.Server.SERVERLOCALE}/dir/test"`,()=>{
+        it(`domain(["dir/test","/a${global.Server.SERVERLOCALE}/dir/test"],app) == "/test,${global.Server.SERVERLOCALE}/dir/test"`,(done)=>{
             var string = "",
                 array = ["dir/test",`/a${global.Server.SERVERLOCALE}/dir/test`]
                 
             expect(
                 pm.domain(array,app).toString()
             ).to.equal(`/test,${global.Server.SERVERLOCALE}/dir/test`)
+            done()
         })
     })
 
