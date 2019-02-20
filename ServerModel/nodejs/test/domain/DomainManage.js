06bc2d727e6e5cd96d5f858d7619d261d3c3188d
onetail <onetail@github.com>
2019-02-20 10:55:53 +0800

    update DomainManage test 'done'

--- ServerModel/nodejs/test/domain/DomainManage.js
+++ ServerModel/nodejs/test/domain/DomainManage.js
@@ -6,21 +6,25 @@ const domain = require("../../lib/domain/DomainManage")
 describe('DomainManage',()=>{
     describe(`#exec 的內部功能`,()=>{
 
-        it(`open mysql & mongoDb database module in server`,()=>{
+        it(`open mysql & mongoDb database module in server`,(done)=>{
             domain.databaseMethod()
+            done()
         })
 
-        it(`open socket module in server`,()=>{
+        it(`open socket module in server`,(done)=>{
             domain.socketMethod()
+            done()
         })
 
-        it(`build http and https server`,()=>{
+        it(`build http and https server`,(done)=>{
             domain.listen()
+            done()
         })
 
-        it(`http method 'get' url component`,()=>{
+        it(`http method 'get' url component`,(done)=>{
             domain.getMethod(["ob","oc"],1)
             domain.getMethod(["dd","bb"],2)
+            done()
         })
     })
 })
