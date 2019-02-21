cc035b918ccd1488a23d5a4ab29a89e2ad25dfab
onetail <onetail@github.com>
2019-02-21 15:47:44 +0800

    finished Filepath.js test

--- ServerModel/nodejs/test/path/Filepath.js
+++ ServerModel/nodejs/test/path/Filepath.js
@@ -6,9 +6,40 @@ const global = require(path.join(__dirname,"../../../../Assets/global/Global"))
 
 describe(`Filepath`,()=>{
     
-    describe(`\t#exec`,()=>{})
-    describe(`\t#isPostFile`,()=>{})
-    describe(`\t#getLocate`,()=>{})
-    describe(`\t#postLocate`,()=>{})
-    describe(`\t#clsLocate`,()=>{})
+    describe(`\t#exec`,()=>{
+        it(` -> lib/path getLocate['lib/path/Filepath.js'] postLocate[]`,(done)=>{
+            fp.exec("lib/path")
+            expect(fp.getLocate().toString()).to.equal("lib/path/Filepath.js")
+            expect(fp.postLocate().toString()).to.equal("")
+            done()
+        })
+    })
+    describe(`\t#isPostFile`,()=>{
+        it(`post.js === false`,(done)=>{
+            expect(
+                fp.isPostFile("post.js")
+            ).to
+            .false
+            done()
+        })
+
+        it(`${global.POSTFILEENDWITH} === true`,(done)=>{
+
+            expect(
+                fp.isPostFile(global.POSTFILEENDWITH[0])
+            ).to.true
+            done()
+        })
+    })
+    describe(`\t#clsLocate`,()=>{
+        it(``,(done)=>{
+            fp.exec("../../")
+            fp.clsLocate()
+            expect(
+                fp.getLocate().toString() + fp.postLocate().toString()
+            ).to
+            .equal("")
+            done()
+        })
+    })
 })
