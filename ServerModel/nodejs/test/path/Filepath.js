ef545ec9f8b6c504ac88c73fc8159039137e4c33
onetail <onetail@github.com>
2019-02-21 14:38:24 +0800

    create new file Filepath.js for test

---
+++ ServerModel/nodejs/test/path/Filepath.js
@@ -0,0 +1,14 @@
+const chai = require("chai")
+var expect = chai.expect
+const path = require("path")
+const fp = require("../../lib/path/Filepath")
+const global = require(path.join(__dirname,"../../../../Assets/global/Global"))
+
+describe(`Filepath`,()=>{
+    
+    describe(`\t#exec`,()=>{})
+    describe(`\t#isPostFile`,()=>{})
+    describe(`\t#getLocate`,()=>{})
+    describe(`\t#postLocate`,()=>{})
+    describe(`\t#clsLocate`,()=>{})
+})
