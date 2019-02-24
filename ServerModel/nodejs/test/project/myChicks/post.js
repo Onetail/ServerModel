dcd5d566a2293c38523d7d105fdbbe6e959edf95
onetail <onetail@github.com>
2019-02-24 11:59:25 +0800

    add myChicksTest test

---
+++ ServerModel/nodejs/test/project/myChicks/post.js
@@ -0,0 +1,15 @@
+const chai = require("chai")
+const chaiHttp = require("chai-http")
+const path = require("path")
+
+var expect = chai.expect
+
+const project = require("../../../lib/project/myChicks/post")
+const global = require(path.join(__dirname,"../../../../Assets/global/Global"))
+
+const express = require("express")
+const http = require("http")
+
+describe(`myChicks Test`,()=>{
+    
+})
