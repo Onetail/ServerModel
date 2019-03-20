c571f74c1c4132d2c2a181c5e2396c7bd5660011
onetail <onetail@github.com>
2019-03-20 16:36:43 +0800

    add docker-compose use

---
+++ sql/Server.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE  IF NOT EXISTS `ServerModel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `ServerModel`;
+-- MySQL dump 10.13  Distrib 8.0.13, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: bbs-dev
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
